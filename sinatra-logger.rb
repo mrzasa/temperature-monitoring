c3e69496591f2242916864af6d65df301748a1d8
Maciej Rzasa <maciejrzasa@gmail.com>
2016-01-17 00:20:15 +0100

    ignored env

--- sinatra-logger.rb
+++ sinatra-logger.rb
@@ -5,8 +5,12 @@ get '/events/new' do
 end
 
 post '/events' do
-  File.open('temperature.log', 'a+') do |f|
+  File.open('temperature.txt', 'a+') do |f|
     f.puts "#{Time.now} temperature=#{params[:temperature]}"
   end
   redirect '/events/new'
+end
+
+get '/events' do
+  send_file('temperature.txt')
 end
